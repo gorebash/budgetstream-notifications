3140d6cf95f11df82c90c4d2b012a9eab1dd1360
Brent Gore <brent.gore@gmail.com>
2023-08-26 11:36:31 -0700

    Saves notification subscription data onto user records

--- BudgetStream.Notifications/Functions/Subscribe.cs
+++ BudgetStream.Notifications/Functions/Subscribe.cs
@@ -1,47 +1,48 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using BudgetStream.Notifications.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using WebPush;
-using System.Linq;
-using BudgetStream.Notifications.Models;
-using System.Text.Json;
 using Newtonsoft.Json;
-using System.Transactions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using WebPush;
 
 namespace BudgetStream.Notifications.Functions
 {
     public static class Subscribe
     {
-        // todo: temp memory store -- replace with saving to db.
+        // Temp memory store -- eventually replace with saving to db.
         private static List<PushSubscription> _subs = new List<PushSubscription>();
 
 
         [FunctionName(nameof(AddSubscriber))]
         public static async Task<IActionResult> AddSubscriber(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "BudgetStream",
+                collectionName: "Users",
+                ConnectionStringSetting = "UserStore.ConnectionString")]IAsyncCollector<User> userDocumentOut,
             ILogger log)
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic payload = JsonConvert.DeserializeObject(requestBody);
+            
+            // bind subscription properties
+            var sub = MapFrom(payload.subscription);
 
-            // todo: convert to JsonSerializer async.
-            dynamic rawBody = JsonConvert.DeserializeObject(requestBody);
+            // bind user document properties
+            var user = JsonConvert.DeserializeObject<User>(requestBody);
+            user.NotificationSubscription = sub;
 
-            var sub = MapFrom(rawBody);
-
-            // todo: bail if sub aready exists.
-
-            // temp limiter for POC, eventually replace with db.
-            if (_subs.Count > 10)
-                throw new Exception("Subscriber queue is full.");
-
+            // By saving locally it enables test notifications until the notifications trigger also pulls from cosmos.
             _subs.Add(sub);
 
+
+            await userDocumentOut.AddAsync(user);
             return new OkResult();
         }
 
@@ -63,12 +64,19 @@ namespace BudgetStream.Notifications.Functions
             return new OkResult();
         }
 
-
+        /// <summary>
+        /// Transactions are enqueued from Plaid webhook. The function will trigger and send notifications.
+        /// </summary>
+        /// <param name="transaction"></param>
+        /// <param name="log"></param>
         [FunctionName(nameof(SendNotifications))] // rename to transactionAdded
         public static void SendNotifications(
             [QueueTrigger("transactions-queue", Connection = "TransactionsQueue.ConnectionString")] string transaction,
             ILogger log)
         {
+            // todo: retrieve user document from cosmos musing transaction detail.
+            // need to write the sql binding to tie transaction to document using the subscription body.
+
             log.LogInformation($"{nameof(SendNotifications)} queue triggered with {_subs.Count} subscribers.");
 
             SendPushNotification(transaction);
@@ -86,7 +94,6 @@ namespace BudgetStream.Notifications.Functions
 
         private static void SendPushNotification (string transaction)
         {
-            // todo: move to injected
             var pushClient = new WebPushClient();
 
             var privateKey = Environment.GetEnvironmentVariable("VAPID_PRIVATE_KEY");
@@ -107,6 +114,8 @@ namespace BudgetStream.Notifications.Functions
                 PublicKey = publicKey,
                 Subject = subject
             };
+
+            // todo: add user filtering
 
             /* todo: shape notification data using service worker sdk:
             {
---
+++ BudgetStream.Notifications/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebPush;
+
+namespace BudgetStream.Notifications.Models
+{
+    public class User
+    {
+        [JsonProperty("id")]
+        public string DocumentId { get; set; }
+
+        [JsonProperty("pk")]
+        public Pk PartitionKey { get; set; } = new Pk();
+
+        [JsonProperty("subscription")]
+        public PushSubscription NotificationSubscription { get; set; }
+
+        [JsonProperty("fiKeys")]
+        public List<FiKeys> FiKeys { get; set; } = new List<FiKeys>();
+    }
+
+    public class Pk
+    {
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+    }
+
+    public class FiKeys
+    {
+        public string AccessToken { get; set; }
+        public string ItemId { get; set; }
+        public string Cursor { get; set; }
+    }
+}
